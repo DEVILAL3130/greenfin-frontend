55c3330a1d224b6270a41385ecd61b395e61bcae
Devilal Gupta <143402714+DEVILAL3130@users.noreply.github.com>
2026-01-14 22:27:10 +0530

    update api

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,36 @@
 
 
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "https://greenfin-backend-2.onrender.com",
+// });
+
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) config.headers.Authorization = `Bearer ${token}`;
+//   return config;
+// });
+
+// export default api;
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://greenfin-backend-2.onrender.com",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  withCredentials: true,
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
-});
+// Attach token automatically
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
+
