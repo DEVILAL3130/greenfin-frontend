ec59223d2e60e65c90c3ab71aa269131a7fc730d
Devilal Gupta <143402714+DEVILAL3130@users.noreply.github.com>
2026-01-15 00:35:37 +0530

    Refactor API setup and update token handling

--- src/services/api.js
+++ src/services/api.js
@@ -1,36 +1,17 @@
-
-
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "https://greenfin-backend-2.onrender.com",
-// });
-
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("token");
-//   if (token) config.headers.Authorization = `Bearer ${token}`;
-//   return config;
-// });
-
-// export default api;
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL + "/api",
   withCredentials: true,
 });
 
-// Attach token automatically
-api.interceptors.request.use(
+API.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
+    if (token) config.headers.Authorization = `Bearer ${token}`;
     return config;
   },
   (error) => Promise.reject(error)
 );
 
-export default api;
-
+export default API;
